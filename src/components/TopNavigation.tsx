8ad20a651d71c7d75f36fdac9d2e9526c34dc6b8
TharinduDeshananda <tdeshananda@gmail.com>
2024-12-20 21:41:57 +0530

    link changes

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -7,12 +7,12 @@ export default function TopNavigation() {
       <NavItem link="/" title="Home" />
       <NavItem link="/about" title="About" />
       <NavItem link="/news" title="News" />
-      <NavItem link="courses" title="Courses" />
-      <NavItem link="institutions" title="Institutions" />
-      <NavItem link="contact" title="Contact" />
-      <NavItem link="reports" title="Reports" />
-      <NavItem link="faq" title="FAQ" />
-      <NavItem link="vacancies" title="Vacancies" />
+      <NavItem link="/courses" title="Courses" />
+      <NavItem link="/institutions" title="Institutions" />
+      <NavItem link="/contact" title="Contact" />
+      <NavItem link="/reports" title="Reports" />
+      <NavItem link="/faq" title="FAQ" />
+      <NavItem link="/vacancies" title="Vacancies" />
     </div>
   );
 }
